951b2a63f60819b623705dabd505e915c22356f4
midiraj <shekh.deep@gmail.com>
2018-09-08 13:18:31 +0600

    Laravel Resouce

---
+++ routes/api.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Http\Request;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+
+// List articles
+Route::get('articles', 'ArticleController@index');
+// Single Article
+Route::get('articles/{id}', 'ArticleController@show');
+// Create New Article
+Route::post('article', 'ArticleController@store');
+// Update Article
+Route::put('article', 'ArticleController@store');
+// Delete Article
+Route::delete('article/{id}', 'ArticleController@destroy');
